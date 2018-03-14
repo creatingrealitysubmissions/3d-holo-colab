21d96fcdc9ea6a8937fefb1a92b112e514377e0b
Kyle Chadwick <kylepchad@gmail.com>
2018-03-14 02:53:16 -0700

    rotate toolbox and updated car

---
+++ holoColab/Assets/RotateObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    bool buttonDown = false;
+
+    [SerializeField]
+    float rotateSpeed = 10f;
+
+    public GameObject ObjectToRotate;
+
+    public GameObject ButtonText; // to be used to switch button text from Rotate to Stop
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonDown)
+        {
+            ObjectToRotate.transform.Rotate(new Vector3(0, rotateSpeed*Time.deltaTime, 0));
+        }
+    }
+
+    public void ToggleRotate()
+    {
+        if (buttonDown == false)
+        {
+            buttonDown = true;
+        }
+        else
+        {
+            buttonDown = false;
+        }
+    }
+}
